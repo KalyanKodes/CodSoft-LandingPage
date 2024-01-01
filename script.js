cd024ef5e43a14805366a28e838eea24d5b6af90
Codewithharry <asl;dkfslkdkfj@gmail.com>
2024-01-01 18:03:28 +0530

    Added final changes for header

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const sideBarContainer = document.querySelector('.header-nav-items');
 const hamburgerSymbol = document.querySelector('.fa-bars');
 const crossSymbol = document.querySelector('.fa-circle-xmark');
 
-hamburgerSymbol.addEventListener('click' , ()=>{
-    sideBarContainer.classList.toggle('show')
+
+const symbols = [hamburgerSymbol , crossSymbol];
+
+
+symbols.forEach((symbol)=>{
+    symbol.addEventListener('click' , ()=>{
+        sideBarContainer.classList.toggle('show')
+    })
 })
 
-crossSymbol.addEventListener('click' , ()=>{
-    sideBarContainer.classList.toggle('show')
-})
